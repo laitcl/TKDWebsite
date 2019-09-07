10a2e9f34859eff838bfd4a8a9a6a93fa6aabebe
laitcl <laitcl>
2019-09-07 00:37:42 -0400

    Bug fixes on index template, some url changes

--- tkdsite/tkdsite/urls.py
+++ tkdsite/tkdsite/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('home/admin/', admin.site.urls),
     path('home/', include('tkdwebsite.urls')),
     path(r'^tinymce/', include('tinymce.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
