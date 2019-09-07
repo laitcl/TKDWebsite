27c76f6ead67af3227abda62e281e06fa50c834c
laitcl <laitcl>
2019-09-07 01:05:04 -0400

    Further changed urls files to contain raw python string literals

--- Scripts/django/tkdsite/tkdsite/urls.py
+++ Scripts/django/tkdsite/tkdsite/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = patterns('', 
-    url('admin/', admin.site.urls),
-    url('', include('tkdwebsite.urls')),
+    url(r'^admin/', admin.site.urls),
+    url(r'^', include('tkdwebsite.urls')),
     url(r'^tinymce/', include('tinymce.urls')),
 )
